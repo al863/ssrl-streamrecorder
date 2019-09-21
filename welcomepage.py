fc8988d32646c4258ef01f149d65163fcfe139a1
al863 <32818531+al863@users.noreply.github.com>
2019-09-21 15:03:10 -0400

    Update welcomepage.py

--- welcomepage.py
+++ welcomepage.py
@@ -1,7 +1,7 @@
 
 def help():
     print("\n\n\n~ ~ ~ W E L C O M E  T O  S T R E A M R E C O R D E R ! ~ ~ ~ \n")
-    print("This script was created to allow outside camera(s) to simultaneously livestream, record videos, and take pictures = of progress in the cleanroom. Only one camera will be utilized by the script at any time (feel free to make edits to the script if other functions need to be added).\n") 
+    print("This script was created to allow outside camera(s) to simultaneously livestream, record videos, and take pictures of progress in the cleanroom. Only one camera will be utilized by the script at any time (feel free to make edits to the script if other functions need to be added).\n") 
     print("Possible Inputs:\n's'-start recording video\n'q'-stop recording video\n' '(spacebar)-take picture\nesc(esc key)-exit from script\n")
 
 def directions1():
