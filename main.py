98355368c13ced7f6c236ebb7334c8fa05ed144e
al863 <32818531+al863@users.noreply.github.com>
2019-09-21 15:05:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import options
 import welcomepage
 import videorecord
 
-#by @Annie Lin, Lambert HS Class of 2020 (for summer yd 2019)
+#by @Annie Lin, Lambert HS Class of 2020 (for summer 2019 Young Dawg program)
 
 def start():
     welcomepage.help()
